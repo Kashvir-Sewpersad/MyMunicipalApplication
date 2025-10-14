b9ef15cd5f764b21f8f19e3d0df0312140243851
kashc <kashc@THE_BEAST>
2025-10-14 11:44:42 +0200

    commented the issue.cs

--- Models/Issue.cs
+++ Models/Issue.cs
@@ -1,30 +1,48 @@
-﻿//--------------------------- start of file -----------------------------//
+﻿
+
+//********************************************************* start of file ***********************************************************//
+
+//------------------------------ start of imports ---------------------------------//
+
 using System;
 using System.ComponentModel.DataAnnotations;
+
+//---------------------------------- end of imports --------------------------------//
 
 namespace Programming_7312_Part_1.Models
 {
     public class Issue
     {
-        public int Id { get; set; }
+        public int Id { get; set; } // primary key getter and setter
 
         [Required]
-        public string? Location { get; set; }
+        public string? Location { get; set; } // location getter and setter variable 
 
         [Required]
-        public string? Category { get; set; } 
+        public string? Category { get; set; }  // category getter and setter variable
 
         [Required]
-        public string? Description { get; set; } 
+        public string? Description { get; set; }  // description getter and setter variable
 
         public string? AttachedFilePath { get; set; } // optional attachment
 
-        public string Status { get; set; } = "Pending";
+        public string Status { get; set; } = "Pending"; // default status is pending 
 
         public DateTime ReportedDate { get; set; } // to be set to current date and time off submission
 
         // For voting system
-        public int Upvotes { get; set; } = 0;
+        
+        /*
+         *
+         * below method is used to track the number of upvotes an issue has had
+         *
+         * this output from the data set to this is used to determine how popular a search is
+         *
+         * this will be used ti recommend issues based on what is popular
+         *
+         * 
+         */
+        public int Upvotes { get; set; } = 0; // default set to zero 
 
         public Issue()
         {
@@ -32,4 +50,4 @@ namespace Programming_7312_Part_1.Models
         }
     }
 }
-//------------------------------------ end of file --------------------------------//
+//**************************************************** end of file ***********************************************************//
